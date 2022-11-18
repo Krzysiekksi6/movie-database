d67834930371db8f5b458a68fd081b188fee44ef
Krzysiek <krzysiekksi6@gmail.com>
2022-11-18 18:09:30 +0100

    navigation v.03

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,28 +5,38 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/NavigationBar.css'
+import './css/NavigationBar.css';
+import logo from './video.svg';
 import { COLORS } from '../values/colors';
 
 const NavigationBar = () => {
+    const brand = (<span className='text text-first'>Film<span className='text text-second'>Web</span></span>);
+    
 	return (
-        <Container style={{color: COLORS.whiteColor}}>
-			<Navbar expand='md' fixed='top' style={{backgroundColor: COLORS.dark}}>
-				<Container className="d-flex justify-content-between">
-					<Navbar.Brand href='#'>Navbar scroll</Navbar.Brand>
-                    <Form>
-							<Form.Control
-								type='search'
-								placeholder='Search'
-								className='me-2'
-								aria-label='Search'
-							/>
-						</Form>
-							<Button variant='outline-success'>Zaloguj się</Button>
-
+		<Container style={{ color: COLORS.whiteColor }}>
+			<Navbar expand='md' fixed='top' style={{ backgroundColor: COLORS.dark }}>
+				<Container className='d-flex justify-content-between'>
+					<Navbar.Brand href='/'>
+						<img
+							src={logo}
+							alt='filweb logo'
+							width='60'
+							height='30'
+						/>{brand}
+						
+					</Navbar.Brand>
+					<Form>
+						<Form.Control
+							type='search'
+							placeholder='Search'
+							className='me-5'
+							aria-label='Search'
+						/>
+					</Form>
+					<Button className='login-btn'>Zaloguj się</Button>
 				</Container>
 			</Navbar>
-            </Container>
+		</Container>
 	);
 };
 
--- src/values/colors.js
+++ src/values/colors.js
@@ -2,5 +2,5 @@ export const COLORS = {
 	dark: '#222831',
 	light: '#393E46',
 	ctaColor: '#00ADB5',
-	whiteColor: 'EEEEEE',
+	whiteColor: '#EEEEEE',
 };
