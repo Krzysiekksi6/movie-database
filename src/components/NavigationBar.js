1e6cb2e21f8b616c955944b91da7fff99f137d24
Krzysiek <krzysiekksi6@gmail.com>
2022-11-18 19:08:03 +0100

    solve problems in nav

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,39 +1,49 @@
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
+import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/NavigationBar.css';
 import logo from './video.svg';
 import { COLORS } from '../values/colors';
 
+
+
+
 const NavigationBar = () => {
-    const brand = (<span className='text text-first'>Film<span className='text text-second'>Web</span></span>);
-    
+	const brand = (
+		<span className='text text-first'>
+			Film<span className='text text-second'>Web</span>
+		</span>
+	);
+
 	return (
 		<Container style={{ color: COLORS.whiteColor }}>
 			<Navbar expand='md' fixed='top' style={{ backgroundColor: COLORS.dark }}>
 				<Container className='d-flex justify-content-between'>
 					<Navbar.Brand href='/'>
-						<img
-							src={logo}
-							alt='filweb logo'
-							width='60'
-							height='30'
-						/>{brand}
-						
+						<img src={logo} alt='filweb logo' width='60' height='30' />
+						{brand}
 					</Navbar.Brand>
-					<Form>
-						<Form.Control
-							type='search'
-							placeholder='Search'
-							className='me-5'
-							aria-label='Search'
-						/>
-					</Form>
-					<Button className='login-btn'>Zaloguj się</Button>
+					<div className='d-flex box'>
+						<Form>
+							<Row>
+								<Col xs="auto">
+									<Form.Control
+										type='search'
+										placeholder='Search'
+										// value={""}
+										aria-label='Search'
+									/>
+								</Col>
+							</Row>
+						</Form>
+						<Button className='btn-nav btn-nav--search'>Szukaj</Button>
+					</div>
+
+					<Button className='btn-nav'>Zaloguj się</Button>
 				</Container>
 			</Navbar>
 		</Container>
