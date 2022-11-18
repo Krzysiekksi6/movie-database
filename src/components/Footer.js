bd57128bddcb15027ac0857d87e8cb4c6f6c5918
Krzysiek <krzysiekksi6@gmail.com>
2022-11-18 19:29:33 +0100

    footer final version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,10 @@
+import './css/Footer.css'
 const Footer = () => {
-	return <div></div>;
+	return (
+        <footer>
+            <p>&copy; 2022 FILM<span>WEB</span> Krzysztof Ksiazek</p>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import NavigationBar from '../components/NavigationBar';
-
+import Footer from '../components/Footer'
 import './css/Home.css';
 
 const Home = () => {
     return(
         <div className='home'>
             <NavigationBar/>
-            
+            <Footer/>
         </div>
     );
 }
