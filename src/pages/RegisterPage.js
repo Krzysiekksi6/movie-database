582593ea9764c40fc5e2b30dbc881151d28a9769
Krzysiek <krzysiekksi6@gmail.com>
2022-11-29 22:39:07 +0100

    login-mobilev.03

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -4,22 +4,34 @@ import { Link } from 'react-router-dom';
 const RegisterPage = () => {
 	return (
 		<div className='register'>
-			<h2 className='login-heading'>Rejestracja</h2>
-
 			<form className='login__form'>
 				<div className='login__form-top'>
+					<h2 className='login-heading'>Rejestracja</h2>
 					<label for='login' class='login__form-label'>
 						Login
 					</label>
-					<input type='text' className='login__form-input' id='login' placeholder='Nickname' />
+					<input
+						type='text'
+						className='login__form-input'
+						id='login'
+						placeholder='Nickname'
+					/>
 					<label for='email' class='login__form-label'>
 						E-mail
 					</label>
-					<input type='email' className='login__form-input' id='email' placeholder='email@example.com'/>
+					<input
+						type='email'
+						className='login__form-input'
+						id='email'
+						placeholder='email@example.com'
+					/>
 					<label for='password' className='login__form-label'>
 						Hasło
 					</label>
-					<input type='password' className='login__form-input' placeholder='*********'></input>
+					<input
+						type='password'
+						className='login__form-input'
+						placeholder='*********'></input>
 					<div className='login__form-btns'>
 						<button type='submit' className='login__form-btn'>
 							Zajerestruj się
